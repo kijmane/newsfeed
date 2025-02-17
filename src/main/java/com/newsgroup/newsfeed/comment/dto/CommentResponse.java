f774c130f73ebe25790388396cae4bc6a0fc7a66
J <kj5592305@gmail.com>
2025-02-17 15:12:11 +0900

    댓글을 작성한 사람만 수정/삭제버튼 보일 수 있도록 수정

--- src/main/java/com/newsgroup/newsfeed/comment/dto/CommentResponse.java
+++ src/main/java/com/newsgroup/newsfeed/comment/dto/CommentResponse.java
@@ -9,12 +9,13 @@ public class CommentResponse {
     private final Long id;
     private final String nickname;
     private final String content;
-    private final boolean canModifyOrDelete; // 댓글 작성자인지 여부 추가
+    private final boolean canModifyOrDelete; // 댓글 작성자인지 여부
 
     public CommentResponse(Comment comment, User currentUser) {
         this.id = comment.getId();
         this.nickname = comment.getUser().getNickname();
         this.content = comment.getContent();
-        this.canModifyOrDelete = comment.getUser().equals(currentUser); // 현재 사용자가 댓글 작성자인지 체크
+        // 현재 사용자가 해당 댓글을 작성한 경우에만 수정/삭제 가능
+        this.canModifyOrDelete = comment.getUser().equals(currentUser);
     }
 }
