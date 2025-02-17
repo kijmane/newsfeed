f58bd864c8b9cb394332a800f32f2e74616f8de5
J <kj5592305@gmail.com>
2025-02-17 14:38:42 +0900

    수정

--- src/main/java/com/newsgroup/newsfeed/NewsfeedApplication.java
+++ src/main/java/com/newsgroup/newsfeed/NewsfeedApplication.java
@@ -2,12 +2,12 @@ package com.newsgroup.newsfeed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling // JPA Auditing 활성화
 public class NewsfeedApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NewsfeedApplication.class, args);
     }
-
 }
--- src/main/java/com/newsgroup/newsfeed/comment/dto/CommentResponse.java
+++ src/main/java/com/newsgroup/newsfeed/comment/dto/CommentResponse.java
@@ -4,21 +4,17 @@ import com.newsgroup.newsfeed.comment.entity.Comment;
 import com.newsgroup.newsfeed.comment.entity.User;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-
 @Getter
 public class CommentResponse {
     private final Long id;
     private final String nickname;
     private final String content;
-    private final LocalDateTime createdAt;
     private final boolean canModifyOrDelete; // 댓글 작성자인지 여부 추가
 
     public CommentResponse(Comment comment, User currentUser) {
         this.id = comment.getId();
         this.nickname = comment.getUser().getNickname();
         this.content = comment.getContent();
-        this.createdAt = comment.getCreatedAt();
         this.canModifyOrDelete = comment.getUser().equals(currentUser); // 현재 사용자가 댓글 작성자인지 체크
     }
 }
