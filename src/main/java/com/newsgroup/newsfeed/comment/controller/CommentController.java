dd95d946eb83e7fddd2ceb2f38c2afbf65548c80
Your Name <your.email@example.com>
2025-02-17 12:59:30 +0900

    CommentController 수정

--- src/main/java/com/newsgroup/newsfeed/comment/controller/CommentController.java
+++ src/main/java/com/newsgroup/newsfeed/comment/controller/CommentController.java
@@ -1,6 +1,5 @@
 package com.newsgroup.newsfeed.comment.controller;
 
-import com.newsgroup.newsfeed.comment.dto.CommentRequest;
 import com.newsgroup.newsfeed.comment.dto.CommentResponse;
 import com.newsgroup.newsfeed.comment.entity.Comment;
 import com.newsgroup.newsfeed.comment.repository.CommentRepository;
@@ -8,7 +7,6 @@ import com.newsgroup.newsfeed.comment.service.CommentService;
 import com.newsgroup.newsfeed.post.entity.Post;
 import com.newsgroup.newsfeed.comment.repository.PostRepository;
 import com.newsgroup.newsfeed.user.entity.User;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -20,22 +18,12 @@ import java.util.List;
 @RequestMapping("/api/comments")
 @RequiredArgsConstructor
 public class CommentController {
-    // 게시물 주인 + 본인 댓글 수정/삭제 가능
+
     private final CommentService commentService;
     private final PostRepository postRepository;
     private final CommentRepository commentRepository;
 
-    @PostMapping("/{postId}")
-    public ResponseEntity<CommentResponse> addComment(
-            @AuthenticationPrincipal User user,
-            @PathVariable Long postId,
-            @Valid @RequestBody CommentRequest request
-    ) {
-        Post post = postRepository.findById(postId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다."));
-        return ResponseEntity.ok(commentService.addComment(user, post, request));
-    }
-
+    // 특정 게시물 댓글 목록 조회 ( 본인이 작성한 댓글이거나 본인의 게시물에 달린 댓글만 관리 가능 )
     @GetMapping("/{postId}")
     public ResponseEntity<List<CommentResponse>> getComments(
             @AuthenticationPrincipal User user,
@@ -43,9 +31,11 @@ public class CommentController {
     ) {
         Post post = postRepository.findById(postId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다."));
+
         return ResponseEntity.ok(commentService.getComments(post, user));
     }
 
+    // 댓글 수정 ( 본인 또는 게시물 주인만 가능 )
     @PutMapping("/{commentId}")
     public ResponseEntity<Void> updateComment(
             @AuthenticationPrincipal User user,
@@ -54,10 +44,12 @@ public class CommentController {
     ) {
         Comment comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
+
         commentService.updateComment(user, comment, content);
         return ResponseEntity.ok().build();
     }
 
+    // 댓글 삭제 ( 본인 또는 게시물 주인만 가능 )
     @DeleteMapping("/{commentId}")
     public ResponseEntity<Void> deleteComment(
             @AuthenticationPrincipal User user,
@@ -65,6 +57,7 @@ public class CommentController {
     ) {
         Comment comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
+
         commentService.deleteComment(user, comment);
         return ResponseEntity.ok().build();
     }
