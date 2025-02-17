b9a10eb584fd65a1f694b1ebd00a90567274bede
J <kj5592305@gmail.com>
2025-02-17 13:04:17 +0900

    CommentRequest 수정

--- src/main/java/com/newsgroup/newsfeed/comment/dto/CommentRequest.java
+++ src/main/java/com/newsgroup/newsfeed/comment/dto/CommentRequest.java
@@ -1,12 +1,13 @@
 package com.newsgroup.newsfeed.comment.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class CommentRequest {
-    // 댓글 작성 요청 받을 DTO
-    // 수정 시 새로운 content만 전달받음
+    // 댓글 수정 시 새로운 내용만 전달받음
+    @NotBlank(message = "댓글 내용은 비워둘 수 없습니다.") // 빈 값 방지
     private String content;
 }
