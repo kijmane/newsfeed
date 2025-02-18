d63732047d33cade2107f19ec6ac88b08116215e
J <kj5592305@gmail.com>
2025-02-18 10:58:36 +0900

    예외처리 코드문서화

--- src/main/java/com/newsgroup/newsfeed/exception/CustomException.java
+++ src/main/java/com/newsgroup/newsfeed/exception/CustomException.java
@@ -1,6 +1,7 @@
 package com.newsgroup.newsfeed.exception;
 
 public class CustomException extends RuntimeException {
+    // 예외 클래스 : 예외 발생 시 사용자 정의 메시지 전달
     public CustomException(String message) {
         super(message);
     }
