f75bc3dfc762d1c0a8056776578251a84088519a
Your Name <your.email@example.com>
2025-02-18 10:37:33 +0900

    코드 문서화

--- src/main/java/com/newsgroup/newsfeed/dto/CommentRequest.java
+++ src/main/java/com/newsgroup/newsfeed/dto/CommentRequest.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class CommentRequest {
-    // 댓글 수정 시 새로운 내용만 전달받음
-    @NotBlank(message = "댓글 내용은 비워둘 수 없습니다.") // 빈 값 방지
+    // 댓글 수정 요청 시 전달되는 데이터 - 새로운 댓글 내용만 포함 , 빈 값일 경우 예외 발생
+    @NotBlank(message = "댓글 내용은 비워둘 수 없습니다.")
     private String content;
 }
