210385c2c19c47099198215abada8029a80bf340
J <kj5592305@gmail.com>
2025-02-19 08:48:23 +0900

    Delete Newsfeed directory

--- Newsfeed/src/main/java/com/newsgroup/newsfeed/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.newsgroup.newsfeed.repository;
-
-import com.newsgroup.newsfeed.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-    User findByNickname(String nickname);
-}
