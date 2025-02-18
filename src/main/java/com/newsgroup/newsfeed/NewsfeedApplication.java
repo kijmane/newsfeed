7c3ec2093a301afbb81a32c9dad566b778d61401
Your Name <your.email@example.com>
2025-02-18 14:18:12 +0900

    캐시 문제 해결

--- src/main/java/com/newsgroup/newsfeed/NewsfeedApplication.java
+++ src/main/java/com/newsgroup/newsfeed/NewsfeedApplication.java
@@ -2,10 +2,8 @@ package com.newsgroup.newsfeed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing // JPA Auditing 기능 활성화
 public class NewsfeedApplication {
     public static void main(String[] args) {
         SpringApplication.run(NewsfeedApplication.class, args);
