9b03c94e748584ce8529d47379d77af57dc6f292
J <kj5592305@gmail.com>
2025-02-18 10:37:33 +0900

    코드 문서화

--- src/main/java/com/newsgroup/newsfeed/controller/CommentController.java
+++ src/main/java/com/newsgroup/newsfeed/controller/CommentController.java
@@ -18,6 +18,7 @@ public class CommentController {
 
     private final CommentService commentService;
 
+    // 특정 게시물의 댓글 목록 조회 - 본인이 작성한 댓글 , 본인의 게시물에 달린 댓글만 조회 가능
     @GetMapping("/{postId}")
     public ResponseEntity<List<CommentResponse>> getComments(
             @AuthenticationPrincipal Users user,
@@ -26,6 +27,7 @@ public class CommentController {
         return ResponseEntity.ok(commentService.getComments(postId, user));
     }
 
+    // 댓글 수정 - 댓글 작성자 , 게시물 소유자만 수정 가능
     @PutMapping("/{commentId}")
     public ResponseEntity<Void> updateComment(
             @AuthenticationPrincipal Users user,
@@ -35,7 +37,7 @@ public class CommentController {
         commentService.updateComment(user, commentId, request);
         return ResponseEntity.ok().build();
     }
-
+    // 댓글 삭제 - 댓글 작성자 , 게시물 소유자만 삭제 가능
     @DeleteMapping("/{commentId}")
     public ResponseEntity<Void> deleteComment(
             @AuthenticationPrincipal Users user,
