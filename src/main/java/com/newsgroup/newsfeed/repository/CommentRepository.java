d618720e1d6670fa5a0173e4248212674ba4d750
Your Name <your.email@example.com>
2025-02-19 17:51:14 +0900

    JavaDoc 주석 추가

--- src/main/java/com/newsgroup/newsfeed/repository/CommentRepository.java
+++ src/main/java/com/newsgroup/newsfeed/repository/CommentRepository.java
@@ -4,7 +4,10 @@ import com.newsgroup.newsfeed.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+/*
+    댓글 저장소 인터페이스
+    - 댓글 데이터를 데이터베이스에서 조회 , 저장 , 삭제 제공
+ */
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Page<Comment> findByPostId (Long postId, Pageable pageable);
 }
