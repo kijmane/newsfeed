25247d51486249ce034e9fa0d243524906802b29
Your Name <your.email@example.com>
2025-02-19 12:40:01 +0900

    import 경로 수정

--- src/main/java/com/newsgroup/newsfeed/dto/requestDtos/comment/CommentRequestDto.java
+++ src/main/java/com/newsgroup/newsfeed/dto/requestDtos/comment/CommentRequestDto.java
@@ -5,7 +5,7 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class CommentRequest {
+public class CommentRequestDto {
     @NotBlank(message = "댓글 내용은 비워둘 수 없습니다.")
     private String content;
 }
--- src/main/java/com/newsgroup/newsfeed/service/comment/CommentService.java
+++ src/main/java/com/newsgroup/newsfeed/service/comment/CommentService.java
@@ -1,12 +1,12 @@
 package com.newsgroup.newsfeed.service.comment;
 
-import com.newsgroup.newsfeed.dto.requestDtos.comment.CommentRequest;
-import com.newsgroup.newsfeed.dto.responseDtos.comment.CommentResponse;
+import com.newsgroup.newsfeed.dto.requestDtos.comment.CommentRequestDto;
+import com.newsgroup.newsfeed.dto.responseDtos.comment.CommentResponseDto;
 import com.newsgroup.newsfeed.entity.Users;
 import java.util.List;
 
 public interface CommentService {
-    List<CommentResponse> getComments(java.lang.Long postId, Users currentUser);
-    void updateComment(Users user, java.lang.Long commentId, CommentRequest request);
+    List<CommentResponseDto> getComments(java.lang.Long postId, Users currentUser);
+    void updateComment(Users user, java.lang.Long commentId, CommentRequestDto request);
     void deleteComment(Users user, java.lang.Long commentId);
 }
