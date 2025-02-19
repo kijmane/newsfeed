63f50485fad236a79c302db668da5688d6fd4efb
Your Name <your.email@example.com>
2025-02-19 14:17:23 +0900

    CommentResponse 기본 생성자 사용 제한 최적화

--- src/main/java/com/newsgroup/newsfeed/dto/response/comment/CommentResponse.java
+++ src/main/java/com/newsgroup/newsfeed/dto/response/comment/CommentResponse.java
@@ -2,19 +2,26 @@ package com.newsgroup.newsfeed.dto.response.comment;
 
 import com.newsgroup.newsfeed.entity.Comment;
 import com.newsgroup.newsfeed.entity.Users;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AccessLevel;
+
+import java.util.Objects;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
 public class CommentResponse {
-    private final Long id;
-    private final String nickname;
-    private final String content;
-    private final boolean canModifyOrDelete;
+    private Long id;
+    private String nickname;
+    private String content;
+    private boolean canModifyOrDelete;
 
     public CommentResponse(Comment comment, Users currentUser) {
         this.id = comment.getId();
         this.nickname = comment.getUser().getNickname();
         this.content = comment.getContent();
-        this.canModifyOrDelete = comment.getUser().equals(currentUser);
+        this.canModifyOrDelete = Objects.equals(comment.getUser(), currentUser);
     }
 }
