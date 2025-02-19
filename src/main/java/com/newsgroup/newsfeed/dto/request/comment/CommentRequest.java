10f23ffe21cf70463f858630203b19b323308427
J <kj5592305@gmail.com>
2025-02-19 17:44:17 +0900

    JavaDoc 주석 처리

--- src/main/java/com/newsgroup/newsfeed/dto/request/comment/CommentRequest.java
+++ src/main/java/com/newsgroup/newsfeed/dto/request/comment/CommentRequest.java
@@ -2,7 +2,11 @@ package com.newsgroup.newsfeed.dto.request.comment;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
+/*
+    댓글 작성 및 수정 요청을 위한 DTO
+    - 댓글 작성 시 본 DTO를 이용하여 클라이언트에서 데이터를 전달받음
+    - 댓글 내용 (공백 불가) 필수 입력 값 , 공백일 경우 유효성 검사에 의해 예외 발생
+ */
 @Getter
 @AllArgsConstructor
 public class CommentRequest {
