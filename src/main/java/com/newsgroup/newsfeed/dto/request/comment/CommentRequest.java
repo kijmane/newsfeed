8ac151b3749562e6bf1cdf42acaa1746b37927cd
Your Name <your.email@example.com>
2025-02-19 14:15:11 +0900

    CommentRequest 어노테이션 추가

--- src/main/java/com/newsgroup/newsfeed/dto/request/comment/CommentRequest.java
+++ src/main/java/com/newsgroup/newsfeed/dto/request/comment/CommentRequest.java
@@ -1,10 +1,12 @@
 package com.newsgroup.newsfeed.dto.request.comment;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class CommentRequest {
     @NotBlank(message = "댓글 내용은 비워둘 수 없습니다.")
     private String content;
