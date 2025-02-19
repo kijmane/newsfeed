f276612f783d25c9cf18f150ecbd624ba6a6d356
plusmate <psejin1478@gmail.com>
2025-02-19 09:19:43 +0900

    깃허브 최신화를 위한 커밋

--- src/main/java/com/newsgroup/newsfeed/service/follow/FollowService.java
+++ src/main/java/com/newsgroup/newsfeed/service/follow/FollowService.java
@@ -1,6 +1,7 @@
 package com.newsgroup.newsfeed.service.follow;
 
 import com.newsgroup.newsfeed.dto.requestDtos.follow.FollowReqDto;
+import com.newsgroup.newsfeed.dto.respondDtos.follow.FollowRespDto;
 import com.newsgroup.newsfeed.entity.Follow;
 import com.newsgroup.newsfeed.entity.Users;
 import com.newsgroup.newsfeed.enums.FollowEnum;
@@ -8,7 +9,7 @@ import com.newsgroup.newsfeed.enums.FollowEnum;
 import java.util.List;
 
 public interface FollowService {
-    String follow(FollowReqDto dto);
+    FollowRespDto follow(FollowReqDto dto);
 
     List<Users> searchFollowList(Users targetUser, FollowEnum followEnum);
 
