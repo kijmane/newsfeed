dd6b2e4d80178d570106526d33a02fc78b06d682
J <kj5592305@gmail.com>
2025-02-19 16:44:17 +0900

    댓글 목록 조회 수정

--- src/main/java/com/newsgroup/newsfeed/repository/CommentRepository.java
+++ src/main/java/com/newsgroup/newsfeed/repository/CommentRepository.java
@@ -1,12 +1,10 @@
 package com.newsgroup.newsfeed.repository;
 
 import com.newsgroup.newsfeed.entity.Comment;
-import com.newsgroup.newsfeed.entity.Posts;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    // 기존 findByPostId -> findByPost로 수정
     Page<Comment> findByPostId (Long postId, Pageable pageable);
 }
