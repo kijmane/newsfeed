ed77db9c2d8a452a38f53acb7fea680594cbb263
Your Name <your.email@example.com>
2025-02-19 14:31:52 +0900

    어노테이션 추가

--- src/main/java/com/newsgroup/newsfeed/repository/CommentRepository.java
+++ src/main/java/com/newsgroup/newsfeed/repository/CommentRepository.java
@@ -3,8 +3,11 @@ package com.newsgroup.newsfeed.repository;
 import com.newsgroup.newsfeed.entity.Comment;
 import com.newsgroup.newsfeed.entity.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByPost(Posts post);
 }
