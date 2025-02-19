61cc3d6c5268c78775a1c5273dce024b7df1b05c
J <kj5592305@gmail.com>
2025-02-19 17:47:39 +0900

    필터로직 및 세션방식의 인증 로직 구현

--- src/main/java/com/newsgroup/newsfeed/controller/CommentController.java
+++ src/main/java/com/newsgroup/newsfeed/controller/CommentController.java
@@ -4,13 +4,15 @@ import com.newsgroup.newsfeed.dto.request.comment.CommentRequest;
 import com.newsgroup.newsfeed.dto.response.comment.CommentResponse;
 import com.newsgroup.newsfeed.entity.Users;
 import com.newsgroup.newsfeed.service.comment.CommentService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static com.newsgroup.newsfeed.config.GetLoginUser.getLoginUser;
 
 @RestController
 @RequestMapping("/api/comments")
@@ -24,9 +26,10 @@ public class CommentController {
      */
     @GetMapping("/{postId}")
     public ResponseEntity<List<CommentResponse>> getComments(
-            @AuthenticationPrincipal Users user,
+            HttpSession session,
             @PathVariable Long postId
     ) {
+        Users user = getLoginUser(session);
         List<CommentResponse> comments = commentService.getComments(postId, user);
         return ResponseEntity.ok(comments);
     }
@@ -36,10 +39,11 @@ public class CommentController {
      */
     @PutMapping("/{commentId}")
     public ResponseEntity<CommentResponse> updateComment(
-            @AuthenticationPrincipal Users user,
+            HttpSession session,
             @PathVariable Long commentId,
             @Valid @RequestBody CommentRequest request
     ) {
+        Users user = getLoginUser(session);
         return ResponseEntity.ok(commentService.updateComment(user, commentId, request));
     }
 
@@ -48,9 +52,10 @@ public class CommentController {
      */
     @DeleteMapping("/{commentId}")
     public ResponseEntity<Void> deleteComment(
-            @AuthenticationPrincipal Users user,
+            HttpSession session,
             @PathVariable Long commentId
     ) {
+        Users user = getLoginUser(session);
         commentService.deleteComment(user, commentId);
         return ResponseEntity.noContent().build();
     }
