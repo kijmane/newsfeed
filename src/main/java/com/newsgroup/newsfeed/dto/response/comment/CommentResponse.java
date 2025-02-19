0b7cc70526a772a774d9225b7f72029aee8836b7
J <kj5592305@gmail.com>
2025-02-19 17:46:35 +0900

    JavaDoc 주석 처리

--- src/main/java/com/newsgroup/newsfeed/dto/response/comment/CommentResponse.java
+++ src/main/java/com/newsgroup/newsfeed/dto/response/comment/CommentResponse.java
@@ -8,7 +8,10 @@ import lombok.NoArgsConstructor;
 import lombok.AccessLevel;
 
 import java.util.Objects;
-
+/*
+    댓글 응답 DTO
+    - 클라이언트에 댓글 정보 반환할 때 사용
+ */
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
@@ -18,13 +21,18 @@ public class CommentResponse {
     private String content;
     private boolean canModifyOrDelete;
 
+    /*
+        댓글 엔티티를 기반으로 응답 객체 생성
+     */
     private CommentResponse(Comment comment, Users currentUser) {
         this.id = comment.getId();
         this.nickname = comment.getUser().getNickname();
         this.content = comment.getContent();
         this.canModifyOrDelete = Objects.equals(comment.getUser(), currentUser);
     }
-
+    /*
+        댓글 엔티티를 DTO로 변환하는 정적 메서드
+     */
     public static CommentResponse from(Comment comment, Users currentUser) {
         return new CommentResponse(comment, currentUser);
     }
