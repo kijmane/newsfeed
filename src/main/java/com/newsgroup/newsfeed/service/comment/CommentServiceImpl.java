94be95e51f0d666f82a6a896e02f35e6c87426f9
Your Name <your.email@example.com>
2025-02-19 17:57:04 +0900

    JavaDoc 주석 처리

--- src/main/java/com/newsgroup/newsfeed/service/comment/CommentServiceImpl.java
+++ src/main/java/com/newsgroup/newsfeed/service/comment/CommentServiceImpl.java
@@ -17,12 +17,17 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+/*
+    댓글 관련 서비스 구현체
+ */
 @Service
 @RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
     private final CommentRepository commentRepository;
 
+    /*
+        특정 게시물 댓글 목록 페이징 처리하여 조회
+     */
     @Override
     @Transactional(readOnly = true)
     public List<CommentResponse> getComments(Long postId, Users currentUser, int page, int size, String sortBy, String direction) {
@@ -33,7 +38,9 @@ public class CommentServiceImpl implements CommentService {
                 .map(comment -> CommentResponse.from(comment, currentUser))
                 .collect(Collectors.toList());
     }
-
+    /*
+        댓글 수정 (작성자만 가능)
+     */
     @Override
     @Transactional
     public CommentResponse updateComment(Users user, Long commentId, CommentRequest request) {
@@ -42,7 +49,9 @@ public class CommentServiceImpl implements CommentService {
         comment.updateContent(request.getContent());
         return CommentResponse.from(comment, user);
     }
-
+    /*
+        댓글 삭제 (작성자만 가능)
+     */
     @Override
     @Transactional
     public void deleteComment(Users user, Long commentId) {
@@ -51,11 +60,17 @@ public class CommentServiceImpl implements CommentService {
         commentRepository.delete(comment);
     }
 
+    /*
+        댓글 ID로 댓글 조회 (존재하지 않으면 예외 발생)
+     */
     private Comment getCommentById(Long commentId) {
         return commentRepository.findById(commentId)
                 .orElseThrow(() -> new CustomException(ErrorCode.COMMENT_NOT_FOUND));
     }
 
+    /*
+        댓글 수정 또는 삭제 권한 확인
+     */
     private void checkCommentPermission(Comment comment, Users user) {
         if (comment == null) {
             throw new CustomException(ErrorCode.COMMENT_NOT_FOUND);
