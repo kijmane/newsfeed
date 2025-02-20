b91b10e0ba90d1393d85920777ed490b5881efeb
J <kj5592305@gmail.com>
2025-02-20 12:57:25 +0900

    edit

--- src/main/java/com/newsgroup/newsfeed/controller/CommentController.java
+++ src/main/java/com/newsgroup/newsfeed/controller/CommentController.java
@@ -80,7 +80,7 @@ public class CommentController {
             HttpSession session,
             @PathVariable Long commentId
     ) {
-        Users user = getLoginUser(session); // 세션에서 로그인한 사용자 정보 가져오
+        Users user = getLoginUser(session);
         commentService.deleteComment(user, commentId);
         return ResponseEntity.noContent().build();
     }
